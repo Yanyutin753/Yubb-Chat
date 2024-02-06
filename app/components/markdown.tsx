9bc1034011c6854f12e03b9d3f607ac95f74afe0
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-02-06 15:22:20 +0000

    修改

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -122,14 +122,9 @@ function _MarkDownContent(props: { content: string; imageBase64?: string }) {
     [props.content],
   );
 
-  // 判断文件路径
-  const isImage = (base64: any) => {
-    return base64.endsWith('png.png');
-  };
-
   return (
     <div style={{ fontSize: "inherit" }}>
-      {props.imageBase64 && isImage(props.imageBase64) && <img src={props.imageBase64} alt="" />}
+      {props.imageBase64 && <img src={props.imageBase64} alt="" />}
       <ReactMarkdown
         remarkPlugins={[RemarkMath, RemarkGfm, RemarkBreaks]}
         rehypePlugins={[
@@ -147,7 +142,7 @@ function _MarkDownContent(props: { content: string; imageBase64?: string }) {
           p: (pProps) => <p {...pProps} dir="auto" />,
           a: (aProps) => {
             const href = aProps.href || "";
-            const isInternal = /^\/#/i.test(href);
+            const isInternal = /^/#/i.test(href);
             const target = isInternal ? "_self" : aProps.target ?? "_blank";
             return <a {...aProps} target={target} />;
           },
