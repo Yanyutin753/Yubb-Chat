d4f08348414e150de2ab41bff16ae062739b8908
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-02-06 11:57:59 +0000

    修改

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Yubb's Castle
+          Yubb Castle
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Welcome to magic fairy castle.
