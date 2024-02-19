d9d14920807f2330834b4b7d4443f0e52c943231
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-02-19 05:03:49 +0000

    修改图片

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -515,16 +515,24 @@ export function ImagePreviewer(props: {
   };
 
   const markdownImageUrlCorsProcess = (markdownContent: string) => {
+    const urlCheck = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?'+ // port
+      '(\\/[-a-z\\d%_.~+]*)*'+ // path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
     const updatedContent = markdownContent.replace(
-      /!\[.*?\]\((.*?)\)/g,
-      (match, url) => {
-        if (!url.startsWith("http")) return `![image](${url})`;
+      /!\[(.*?)\]\((.*?)\)/g,
+      (match, alt, url) => {
+        if (!urlCheck.test(url)) return `![${alt}](${url})`;
         const updatedURL = `/api/cors?url=${encodeURIComponent(url)}`;
-        return `![image](${updatedURL})`;
+        return `[image]![${alt}](${updatedURL})`;
       },
     );
     return updatedContent;
   };
+  
 
   return (
     <div className={styles["image-previewer"]}>
